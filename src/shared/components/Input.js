d1b8aa3c225fdd873e557f97a7ca558f00d6ab0b
rajsandhu <rajsandhu@gmail.com>
2019-12-11 12:55:46 +0100

    refactor initial styling

---
+++ src/shared/components/Input.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+    margin: 1rem 0;
+    height: 2.5rem;
+    width: 25rem;
+    padding: 0.5rem 0;
+    border: 0;
+    border-bottom: 1px solid var(--dark-grey);
+    background-color: var(--light-grey);
+
+    &:focus {
+        outline: none;
+        border: 1px solid var(--dark-grey);
+
+    }
+`;
+
+export default Input;
---
+++ src/shared/components/Label.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Label = styled.label`
+    font-size: 2.25rem;
+    font-weight: 600;
+
+`;
+
+export default Label;
