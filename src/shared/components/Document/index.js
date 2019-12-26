ba27ee055ba5db95226b77cbd48b7fd7e6c6ed0b
dyyyl <hey@dyyyl.rocks>
2019-12-26 15:03:48 +0100

    removes extraneous named files

--- src/shared/components/Document/index.js
+++ src/shared/components/Document/index.js
@@ -1,3 +1,14 @@
-import Document from './Document';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Document = ({ userData }) => (
+  <>
+    <h1>{userData.name}</h1>
+  </>
+);
+
+Document.propTypes = {
+  userData: PropTypes.object.isRequired,
+};
 
 export default Document;
